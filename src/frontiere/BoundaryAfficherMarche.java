62b5d1c161f942f77d10023b1a1cb42c2c67593b
Nicolas Bonvalet <bnn3786a@ensfsi.onmicrosoft.com>
2024-03-13 11:53:22 +0100

    2.7 pb out of bounds marche[i]

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,15 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] marche = controlAfficherMarche.donnerInfosMarche();
+		if(marche.length == 0)
+			System.out.println("Le marché est vide comeback later");
+		else {
+			System.out.println(nomAcheteur+" vous trouverez au marché : ");
+			int i=0;
+			while (marche[i] != null) {
+				System.out.println(" - "+marche[i++]+" qui vend "+marche[i++]+" "+marche[i]);
+			}
+		}
 	}
 }
