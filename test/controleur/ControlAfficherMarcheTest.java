70ae1c1bfab7e213b6e8cf03565ba83f8d324b91
Nicolas Bonvalet <bnn3786a@ensfsi.onmicrosoft.com>
2024-03-29 14:24:46 +0100

    Fin ControlAfficherVillageTest && ControlAfficherMarcheTest

--- test/controleur/ControlAfficherMarcheTest.java
+++ test/controleur/ControlAfficherMarcheTest.java
@@ -2,13 +2,26 @@ package controleur;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ControlAfficherMarcheTest {
+import personnages.Chef;
+import villagegaulois.Village;
 
+class ControlAfficherMarcheTest {
+	private Village village;
+	private Chef abraracourcix;
+	@BeforeEach
+	public void initSitu() {
+		System.out.println("Initialisation");
+		village = new Village("le village des irréductibles",10,5);
+		abraracourcix = new Chef("Abraracourcix",10,village);
+		village.setChef(abraracourcix);
+	}
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testdonnerInfosMarche() {
+		ControlAfficherMarche controlAfficherMarche = new ControlAfficherMarche(village);
+		assertNotNull(controlAfficherMarche.donnerInfosMarche());
 	}
 
 }
--- test/controleur/ControlAfficherVillageTest.java
+++ test/controleur/ControlAfficherVillageTest.java
@@ -2,13 +2,44 @@ package controleur;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ControlAfficherVillageTest {
+import personnages.Chef;
+import personnages.Gaulois;
+import villagegaulois.Village;
 
+class ControlAfficherVillageTest {
+	private Village village;
+	private Chef abraracourcix;
+	@BeforeEach
+	public void initSitu() {
+		System.out.println("Initialisation");
+		village = new Village("le village des irréductibles",10,5);
+		abraracourcix = new Chef("Abraracourcix",10,village);
+		village.setChef(abraracourcix);
+		Gaulois julie = new Gaulois("Julie",10);
+		village.ajouterHabitant(julie);
+		village.installerVendeur(julie, null, 0);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testDonnerNomsVillageois() {
+		ControlAfficherVillage controlAfficherVillage = new ControlAfficherVillage(village);
+		String[] nomVillageois = controlAfficherVillage.donnerNomsVillageois();
+		assertEquals("Abraracourcix",nomVillageois[0]);
+		assertEquals("Julie",nomVillageois[1]);
+	}
+	@Test
+	void testDonnerNomVillage() {
+		ControlAfficherVillage controlAfficherVillage = new ControlAfficherVillage(village);
+		assertEquals("le village des irréductibles",controlAfficherVillage.donnerNomVillage());
+		assertNotEquals(null,controlAfficherVillage.donnerNomVillage());
+	}
+	@Test
+	void testDonnerNbEtals() {
+		ControlAfficherVillage controlAfficherVillage = new ControlAfficherVillage(village);
+		assertEquals(5,controlAfficherVillage.donnerNbEtals());
 	}
 
 }
