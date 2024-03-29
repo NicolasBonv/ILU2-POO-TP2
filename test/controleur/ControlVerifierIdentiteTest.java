852ccf6bc0b8100d46f2e44890b3bae98b5125b9
Nicolas Bonvalet <bnn3786a@ensfsi.onmicrosoft.com>
2024-03-29 14:15:19 +0100

    fin ControlVerifierIdentiteTest

--- test/controleur/ControlVerifierIdentiteTest.java
+++ test/controleur/ControlVerifierIdentiteTest.java
@@ -2,13 +2,31 @@ package controleur;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ControlVerifierIdentiteTest {
+import personnages.Chef;
+import personnages.Gaulois;
+import villagegaulois.Village;
 
+class ControlVerifierIdentiteTest {
+	private Village village;
+	private Chef abraracourcix;
+	@BeforeEach
+	public void initSitu() {
+		System.out.println("Initialisation");
+		village = new Village("le village des irréductibles",10,5);
+		abraracourcix = new Chef("Abraracourcix",10,village);
+		village.setChef(abraracourcix);
+	}
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testVerifierIdentite() {
+		Gaulois julie = new Gaulois("Julie",10);
+		village.ajouterHabitant(julie);
+		village.installerVendeur(julie, null, 0);
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		assertTrue(controlVerifierIdentite.verifierIdentite("Julie"));
+		assertFalse(controlVerifierIdentite.verifierIdentite("random"));
 	}
 
 }
